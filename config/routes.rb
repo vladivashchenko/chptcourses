d1b8e520c02fb78ab80596dd49ae6878d93ea271
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-04-03 22:16:01 +0300

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,18 @@ Rails.application.routes.draw do
   resources :groups
   resources :teachers
   resources :users
-  root    'groups#index'
+  root   'static_pages#home'
+  get    '/help',        to: 'static_pages#help'
+  get    '/about',       to: 'static_pages#about'
+  get    '/contact',     to: 'static_pages#contact'
+  get    '/signup',      to: 'users#new'
+  post   '/signup',      to: 'users#create'
+  get    '/login',       to: 'sessions#new'
+  post   '/login',       to: 'sessions#create'
+  delete '/logout',      to: 'sessions#destroy'
+  get     '/groups',     to: 'groups#index'
   get     '/students',   to: 'students#index'
   get     '/teachers',   to: 'teachers#index'
   get     '/subjects',   to: 'subjects#index'
-  get     'signup',      to: 'users#new'
-  get     'login',      to: 'sessions#new'
-  post    'login',      to: 'sessions#create'
-  delete  'logout',     to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

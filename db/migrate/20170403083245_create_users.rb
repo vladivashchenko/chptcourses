64ef0c08ae9d7491153db0186b8d80b407aebe3b
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-04-03 17:44:50 +0300

    Add columns to users table using migration

--- db/migrate/20170403083245_create_users.rb
+++ db/migrate/20170403083245_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
+def change
     create_table :users do |t|
-
-      t.timestamps
+    t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403083245) do
+ActiveRecord::Schema.define(version: 20170403144120) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -45,8 +45,12 @@ ActiveRecord::Schema.define(version: 20170403083245) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end

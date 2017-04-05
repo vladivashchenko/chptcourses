ea2e1f6b8de6ad7db92e0b0d5dd2696ff8b242f2
VivaVlad <vlad.ivashenko.98@mail.ru>
2017-04-05 12:32:23 +0300

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
 
   def index
       @users = User.paginate(page: params[:page])
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Вітаємо на сайті підготовчих курсів ЧПТ!"
       redirect_to @user
     else
       render 'new'
@@ -25,15 +26,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "Профіль оновлений"
       redirect_to @user
     else
       render 'edit'
     end
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
   private
 
@@ -58,4 +65,8 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         redirect_to(root_url) unless current_user?(@user)
       end
-  end
+    # Confirms an admin user.
+        def admin_user
+          redirect_to(root_url) unless current_user.admin?
+        end
+    end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -35,4 +35,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
